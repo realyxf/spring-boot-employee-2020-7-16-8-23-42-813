90806b65cff7485601430359b6f258184266ff80
realyxf <997915421@qq.com>
2020-07-28 18:05:04 +0800

    [Employee] [Fennie,Jay] feature: finish Employee

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private List<Employee> employeeList = new ArrayList<>();
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public List<Employee> getAllEmployee() {
+        return employeeList;
+    }
+
+    public void deleteEmployee(int employeeId) {
+        employeeList.removeIf(employee -> employee.getId() == employeeId);
+    }
+
+    public void updateEmployee(Employee employee) {
+        employeeList.remove(employee.getId());
+        employeeList.add(employee);
+    }
+
+    public void addEmployeeList(List<Employee> inputEmployeeList) {
+        employeeList.addAll(inputEmployeeList);
+    }
+}
